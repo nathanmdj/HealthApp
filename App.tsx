44303ecff1b8f52359294027cd223fe567f91563
nathanmdj <nathanmdj628@gmail.com>
2024-08-17 03:29:41 +0800

    Initialized the App with nativewind, svg and reanimated

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Text, View } from '@/designs/core';
+import CounterStats from './src/components/CounterStats';
+import RingProgress from './src/components/RingProgress';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className='flex-1 justify-center bg-black p-5 w-screen gap-y-10'>
+      <RingProgress progress={0.4} />
+
+      <View className='flex-row gap-20 gap-y-5 flex-wrap'>
+        <View>
+          <CounterStats label='Steps' value={2313} />
+        </View>
+        <View>
+          <CounterStats label='Distance' value={2} unit='km'/>
+        </View>
+        <View>
+          <CounterStats label='Flights Climbed' value={2}/>
+        </View>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
