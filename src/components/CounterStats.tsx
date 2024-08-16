67e2a642aab64b00c10e3d5bfd40cf2fb93b722a
nathanmdj <nathanmdj628@gmail.com>
2024-08-17 03:30:13 +0800

    Created counterstats

---
+++ src/components/CounterStats.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from '@/designs/core';
+
+interface CounterStatsProps {
+  label: string
+  value: number
+  unit?: string
+}
+const CounterStats = ({label, value, unit}: CounterStatsProps) => {
+  return (
+    <View>
+      <Text className='text-white'>{label}</Text>
+      <Text className='text-white text-3xl'>{value} {unit}</Text>
+    </View>
+  )
+}
+
+export default CounterStats
